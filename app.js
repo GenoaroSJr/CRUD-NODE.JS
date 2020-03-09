a8a9215a52f3d5808ed7dfe233a10d2681a7496c
GenoaroSJr <genoarojr@unifei.edu.br>
2020-03-09 06:54:32 -0300

    Esqueleto do projeto. Pastas view, admin, layouts, partials, model, public, routes

---
+++ app.js
@@ -0,0 +1,29 @@
+//Carregando módulos
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+const admin = require('./routes/admin');
+const path = require('path'); // manipular pastas e diretorios
+//const mongoose = require('mongoose');
+
+//Configurações
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
+    //Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    //Mongoose
+        //Em breve
+    
+    //Public
+        app.use(express.static(path.join(__dirname,"public")));
+
+//Rotas
+    app.use('/admin', admin); 
+//Outros
+const PORT = 8081; 
+app.listen(PORT, () =>{
+    console.log("Servidor Rodando em http://localhost:8081");
+});
