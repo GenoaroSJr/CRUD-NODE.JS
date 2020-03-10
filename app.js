65796748d53cd127e940a815ba2de9b935e58645
GenoaroSJr <genoarojr@unifei.edu.br>
2020-03-10 19:56:00 -0300

    Arquivo final de criação de modulos e postagens de modulos.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
 
 //Configurações
     // Sessão
@@ -63,6 +65,34 @@ const Postagem = mongoose.model("postagens");
         }).catch((erro)=>{
             req.flash("error_msg", "Houve um error interno!");
             res.redirect("/")
+        })
+    })
+
+    app.get("/categorias", (req,res)=>{
+        Categoria.find().then((categorias)=>{
+            res.render("categorias/index",{categorias: categorias});
+        }).catch((erro)=>{
+            req.flash("error_msg", "Houve um erro interno!");
+            res.redirect("/");
+        })
+    })
+
+    app.get("/categorias/:slug", (req,res)=>{
+        Categoria.findOne({slug: req.params.slug}).then((categoria)=>{
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).then((postagens)=>{
+                    res.render("categorias/postagens",{postagens: postagens, categoria: categoria})
+                }).catch((erro)=>{
+                    req.flash("error_msg", "Esta categoria não existe!");
+                    res.redirect("/");
+                })
+            }else{
+                req.flash("error_msg", "Esta categoria não existe!");
+                res.redirect("/");
+            }
+        }).catch((erro)=>{
+            req.flash("error_msg", "Houve um erro interno!");
+            res.redirect("/");
         })
     })
 
